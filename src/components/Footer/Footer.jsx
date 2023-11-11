1ee751e8c4116d493d93c9c5a2b6415c52c72b02
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-11-11 10:46:03 -0300

    Update: footer footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 export default function Footer(props) {
   return (
     <footer className="footer">
+       <p className='footer__title'>FINAL PROJECT</p>
        <p className='footer__autor'>&copy; GABRIEL GONÇALVES</p>
     </footer>
   );
