eca4bb22a66f584ce0888da72b411447f66844bb
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-11-14 14:11:50 -0300

    Delete: delete files

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(props) {
-  return (
-    <footer className="footer">
-       <p className='footer__title'>FINAL PROJECT</p>
-       <p className='footer__autor'>&copy; GABRIEL GONÇALVES</p>
-    </footer>
-  );
-}
