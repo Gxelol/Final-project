03dbeee6b6559a796b3d102462fca014b2c22139
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-11-14 13:54:16 -0300

    Update: update Weapons

--- src/components/Weapons/Weapons.jsx
+++ src/components/Weapons/Weapons.jsx
@@ -15,7 +15,7 @@ export default function Weapons(props) {
       {weapons.map((weapon) => (
         <div className="weapons__container" key={weapon.uuid}>
           <p className="weapons__name" key={weapon.displayName}>{weapon.displayName}.</p>
-          <img className="weapons__image" key={weapon.displayIcon} src={weapon.displayIcon} alt="" />
+          <img className="weapons__image" key={weapon.displayIcon} src={weapon.displayIcon} alt="weapon" />
           <p className="weapons__category"  key={weapon.category}>{weapon.category.split('EEquippableCategory::').pop()}</p>
         </div>
       ))}
